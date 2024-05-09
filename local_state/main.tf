c2388ff5066e7296d5c5184c4659c0a501c42847
Akanksha <kumari.akanksha7@gmail.com>
2024-05-09 12:12:14 +0530

    code updated and instance is created

--- local_state/main.tf
+++ local_state/main.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-  #required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 # Configure the AWS Provider
@@ -14,35 +14,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Declare the AWS_AMI to be used for creating instance
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# Create instance
+resource "aws_instance" "app_server" {
+  ami = "ami-07caf09b362be10b8"
+  instance_type  = "t2.micro"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
-
- /* filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}*/
-
-# Use the created AMI that will start a EC2 machine
-resource "aws_instance" "example_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
+   tags = {
     Name = "Terraform_Demo"
-  }
 }
-
-/* Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}*/
+}
